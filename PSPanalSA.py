0ce51cede759c50b6d676396980866362e108403
Avrama Blackwell <kblackw1@GMU.EDU>
2021-05-26 09:20:33 -0400

    PSPanalSA: convert points to ints

--- PSPanalSA.py
+++ PSPanalSA.py
@@ -7,7 +7,6 @@
 #outputs consist of pickle file with dictionary containing input arguments and extracted features
 #outputs also include a text file of TMP, PSP amplitude, hyperpolarization during current injection versus trace ID (to plot single experiment result)
 import os
-import pylab
 from igor import binarywave
 import numpy as np
 from matplotlib import pyplot
@@ -35,9 +34,9 @@ dt=100e-6       # interval between samples (1/sampling frequency)
 plotstart=0 # change to "baseendtime/dt" to zoom in on EPSP in plots
 
 #Looks for files using pattern to find all files within a single exper
-outputDir="Pickle/"
+outputDir="G:/"
 filenameending="_SLH_1_*_*_1p1.ibw"
-inputDir="PatchData/"
+inputDir="G:/PatchData/"
 ##########################################################################
 ######################### Experiment type specific parameters
 
@@ -45,19 +44,22 @@ inputDir="PatchData/"
 parser = argparse.ArgumentParser()
 parser.add_argument('experiment', type=str, help = 'give exp name, for example: 12-Jun-2014_SLH006')
 parser.add_argument('--no-graphs', '-g', dest='graphs', default=True, action='store_false') # -g optional and not position-defined (its absence OK too)
-parser.add_argument("sex", type=str, choices=["M","F"],help="male M or female F")
-parser.add_argument("age", type=int, help="animal age in days")
-parser.add_argument("bathtemp", type=str, choices=["heat","RT"],help="heat or RT")
-parser.add_argument("drug", type=str, choices=["nodrug","NorBNI","Nomi","CGP","other"],help="nodrug,NorBNI,Nomi,CGP,other")
-parser.add_argument("Rtip", type=float,help="pipette tip resistance")
-parser.add_argument("indtime", type=int,help="minutes between break-in and TBS starting")
-parser.add_argument("genotype", type=str, choices=["D1+", "A2a+", "D1-", "A2a-", "wt"],help="D1+ A2a+ (or-) for cre lines, or wt")
-parser.add_argument("celltype", type=str, choices=["MSN", "FSI", "other"],help="MSN, FSI, or other cell type")
-parser.add_argument("lightresp", type=str, choices=["LR", "non"],help="was cell light responsive? (LR or non)")
-parser.add_argument("lightlevel", type=int, help="what light level was used? 0-100")
-parser.add_argument("depol", type=str, choices=["soma", "soma0", "opto"],help="depolarization during TBS: soma, soma0, or opto")
-parser.add_argument("TBSAP", type=str, choices=["APs","noAPs"],help="during TBS, APs or noAPs")
+parser.add_argument("-sex", type=str, choices=["M","F"],help="male M or female F")
+parser.add_argument("-age", type=int, help="animal age in days")
+parser.add_argument("-bathtemp", type=str, choices=["heat","RT"],help="heat or RT", default='RT')
+parser.add_argument("-drug", type=str, choices=["nodrug","NorBNI","Nomi","CGP","other"],help="nodrug,NorBNI,Nomi,CGP,other", default='nodrug')
+parser.add_argument("-Rtip", type=float,help="pipette tip resistance")
+parser.add_argument("-indtime", type=int,help="minutes between break-in and TBS starting")
+parser.add_argument("-genotype", type=str, choices=["D1+", "A2a+", "D1-", "A2a-", "wt"],help="D1+ A2a+ (or-) for cre lines, or wt")
+parser.add_argument("-celltype", type=str, choices=["MSN", "FSI", "other"],help="MSN, FSI, or other cell type", default='MSN')
+parser.add_argument("-lightresp", type=str, choices=["LR", "non"],help="was cell light responsive? (LR or non)", default='non')
+parser.add_argument("-lightlevel", type=int, help="what light level was used? 0-100", default=0)
+parser.add_argument("-depol", type=str, choices=["soma", "soma0", "opto"],help="depolarization during TBS: soma, soma0, or opto")
+parser.add_argument("-TBSAP", type=str, choices=["APs","noAPs"],help="during TBS, APs or noAPs")
 keydict={"sex":["M","F"],"bathtemp":["heat","RT"],"drug":["nodrug","NorBNI","Nomi","CGP","Naloxone","other"],"genotype":["D1+", "D2+", "D1-", "D2-", "wt"],"celltype":["MSN", "FSI", "other"],"lightresp":["LR", "non"],"depol":["soma", "soma0", "opto"],"TBSAP":["APs","noAPs"]}
+
+######### Temporary, to make it work more easily in spyder
+ARGS='30-Jan-2015_SLH001_NonLgtNorBNI -sex M -age 21 -Rtip 5 -indtime 10 -depol soma -TBSAP APs'
 
 with open('choicedict.txt', "wb") as f:# opens the file, for writing 
 	pickle.dump(keydict, f) # writes the keydict to an external file named choicedict 
@@ -78,46 +80,45 @@ except SystemExit:
 		raise ValueError('invalid ARGS')
 
 #print experiment characteristics to double check unconstrained entries
-print "sex={}".format(args.sex)
-print "age={}".format(args.age)
-print "bathtemp={}".format(args.bathtemp)
-print "drug={}".format(args.drug)
-print "Rtip={}".format(args.Rtip)
-print "indtime={}".format(args.indtime)
-print "genotype={}".format(args.genotype)
-print "celltype={}".format(args.celltype)
-print "lightresp={}".format(args.lightresp)
-print "lightlevel={}".format(args.lightlevel)
-print "depol={}".format(args.depol)
-print "TBSAP={}".format(args.TBSAP)
+print( "sex={}".format(args.sex))
+print( "age={}".format(args.age))
+print( "bathtemp={}".format(args.bathtemp))
+print( "drug={}".format(args.drug))
+print( "Rtip={}".format(args.Rtip))
+print( "indtime={}".format(args.indtime))
+print( "genotype={}".format(args.genotype))
+print( "celltype={}".format(args.celltype))
+print( "lightresp={}".format(args.lightresp))
+print( "lightlevel={}".format(args.lightlevel))
+print( "depol={}".format(args.depol))
+print( "TBSAP={}".format(args.TBSAP))
 
 FileDir=inputDir+args.experiment+"_Waves/"
 parts=args.experiment.split('-')
 pattern=FileDir+"W"+parts[0]+"_"+parts[1]+"_"+parts[2].split("_")[0]+filenameending
-print "Looking for files using: ", pattern
+print( "Looking for files using: ", pattern)
 filenames = glob.glob(pattern)
 if (len(filenames)==0):
-	print "You mistyped the filenames. Python found no such files:"
+	print( "You mistyped the filenames. Python found no such files:")
 	pp(filenames)
 
 #Below puts your files from common exper in order by PGF and trace
 def sortorder(fname):
     parts = fname.split('_')
     ans = int(parts[-3]), int(parts[-2])
-    #print 'here:', fname, '->', ans
     return ans
 filenames = sorted(filenames, key=sortorder)#sorted is alphabetical unless integers given
                      #key is output of function "sortorder," using argument "filenames"
 #pp(filenames)
 
 #convert times into points
-basestartpnt=basestarttime/dt
-baseendpnt=baseendtime/dt
-PSPstartpnt=PSPstart/dt
-PSPendpnt=PSPend/dt
-PP2startpnt=PP2start/dt          #<----------------------------------------------
-hyperstartpnt=hyperstart/dt
-hyperendpnt=hyperend/dt
+basestartpnt=int(basestarttime/dt)
+baseendpnt=int(baseendtime/dt)
+PSPstartpnt=int(PSPstart/dt)
+PSPendpnt=int(PSPend/dt)
+PP2startpnt=int(PP2start/dt)          #<----------------------------------------------
+hyperstartpnt=int(hyperstart/dt)
+hyperendpnt=int(hyperend/dt)
 
 #additional measures: tracetime
 #initialize the arrays that will contain the data from all series and traces
@@ -159,15 +160,15 @@ for i,filename in enumerate(filenames):
         trace=data['wave']['wData']
         Vm = np.mean(trace[basestartpnt:baseendpnt]) 
         tempRMP.append(Vm)
-	access=(tempRMP[i]-np.mean(trace[hyperstartpnt:hyperendpnt]))/Iaccess/1e6 #converts access units to megaohms
-	tempAccess.append(access)
+        access=(tempRMP[i]-np.mean(trace[hyperstartpnt:hyperendpnt]))/Iaccess/1e6 #converts access units to megaohms
+        tempAccess.append(access)
 
         if (i==9): 
             baselineVm=np.mean(tempRMP) #this is mean over traces
-	    baselineAccess=np.mean(tempAccess)
+            baselineAccess=np.mean(tempAccess)
 
-	if i>=10:
-	    if (abs(Vm-baselineVm)/abs(baselineVm))> 0.2 or (abs(access-baselineAccess)/abs(baselineAccess))>.4: #20% baseline change or 40% access change
+        if i>=10:
+            if (abs(Vm-baselineVm)/abs(baselineVm))> 0.2 or (abs(access-baselineAccess)/abs(baselineAccess))>.4: #20% baseline change or 40% access change
                 reallybad = True
             else:
                 reallybad = False
@@ -178,68 +179,65 @@ for i,filename in enumerate(filenames):
         elif badcount <= 10: #< 10.  varying would make +- lax criteria -----------
             badcount = 0
             goodtraces = i+1	
-	#print "VM", Vm, "Badcount", badcount,'# goodtraces is ', goodtraces, ', meaning ', (goodtraces/2)-5, ' minutes of follow-up.'
 
-print '# goodtraces is ', goodtraces, ', meaning ', (goodtraces/2)-5, ' minutes of follow-up.'
+print( '# goodtraces is ', goodtraces, ', meaning ', (goodtraces/2)-5, ' minutes of follow-up.')
 
 for fileindex,filename in enumerate(filenames[:goodtraces]):
         data = binarywave.load(filename)      #read in data  
         trace=data['wave']['wData']#wave is NOT x data, but wData is y data.. type "data" into python to explore binary raw data!
         # pp(data) 
-	#wave is key housing dict with key wave_header housing dict with key hsA... which is key linked to dt
+        #wave is key housing dict with key wave_header housing dict with key hsA... which is key linked to dt
         dtfile=data['wave']['wave_header']['hsA']  #verify that the dt we have is correct
         if dt != dtfile:
             raise ValueError("Error, dt of file is different than expected")
         
-#calculate various measures.
+        #calculate various measures.
         RMP.append(np.mean(trace[basestartpnt:baseendpnt]))
 
-	maxvm_i = trace[PSPstartpnt:PSPendpnt].argmax() + PSPstartpnt #argmax gives index (point number) of maximum, within slice
+        maxvm_i = trace[PSPstartpnt:PSPendpnt].argmax() + PSPstartpnt #argmax gives index (point number) of maximum, within slice
         maxvm = trace[maxvm_i]
-	peakvm.append(maxvm)
+        peakvm.append(maxvm)
         peakamp.append(peakvm[fileindex]-RMP[fileindex])
-	maxvm2_i = trace[PSPendpnt:].argmax() + PSPendpnt
-	peaktime.append(maxvm_i*dt) #<------maxvm_i gives point number of maximum.. why multiply by dt??
+        maxvm2_i = trace[PSPendpnt:].argmax() + PSPendpnt
+        peaktime.append(maxvm_i*dt) #<------maxvm_i gives point number of maximum.. why multiply by dt??
 
-	if pp2exists == True:
-	    maxvmpp2_i = trace[PP2startpnt:].argmax() + PP2startpnt
+        if pp2exists == True:
+            maxvmpp2_i = trace[PP2startpnt:].argmax() + PP2startpnt
             maxvmpp2 = trace[maxvmpp2_i]
-	    pp2.append(maxvmpp2)
+            pp2.append(maxvmpp2)
             pp2amp.append(pp2[fileindex]-RMP[fileindex])
-	    pp2time.append(maxvmpp2_i*dt) 
+            pp2time.append(maxvmpp2_i*dt) 
 
         if peak2exists == True:
             maxvm2 = trace[maxvm2_i]
-	    peak2.append(maxvm2)
+            peak2.append(maxvm2)
             peak2time.append(maxvm2_i*dt)
-	    peak2amp.append(peak2[fileindex]-RMP[fileindex])
+            peak2amp.append(peak2[fileindex]-RMP[fileindex])
 
 
-	hypervm.append(np.mean(trace[hyperstartpnt:hyperendpnt]))
+        hypervm.append(np.mean(trace[hyperstartpnt:hyperendpnt]))
         hyperdelta.append(RMP[fileindex]-hypervm[fileindex])
-	AccR.append(hyperdelta[fileindex]/Iaccess)
-    	parts = filename.split('_')
+        AccR.append(hyperdelta[fileindex]/Iaccess)
+        parts = filename.split('_')
         tracestring.append(parts[-3]+"_"+parts[-2]+"   ") #extra spaces forces column_stack to use more significant figures when converting floats to string. 
 
-        #print filename.split('/')[1],tracestring[fileindex],RMP[fileindex],peakvm[fileindex]
         #Optionally, plot the data to verify the results
         endtime=len(trace)*dtfile
         tracetime=np.arange(0,endtime,dt) #array of points between 0 and endtime stepping by dt
         if args.graphs:     
             x=PSPstart
-            #print x
             y_array=np.arange(-.085,-.06)
             x_array=x*np.ones(y_array.shape)
             if fileindex<10:
-	    	axes.plot(tracetime[plotstart:],trace[plotstart:],label=fileindex, color=(0,fileindex*.1, fileindex*.1))
+                axes.plot(tracetime[plotstart:],trace[plotstart:],label=fileindex, color=(0,fileindex*.1, fileindex*.1))
                 axes.plot(tracetime[PSPstartpnt],trace[PSPstartpnt],'rs',ms=10)
-		axes.plot(tracetime[PSPendpnt],trace[PSPendpnt],'rd',ms=10)
-		axes.plot(tracetime[PP2startpnt],trace[PP2startpnt],'gd',ms=10)
+                axes.plot(tracetime[PSPendpnt],trace[PSPendpnt],'rd',ms=10)
+                axes.plot(tracetime[PP2startpnt],trace[PP2startpnt],'gd',ms=10)
             elif fileindex>(goodtraces-11):
-		axes.plot(tracetime[PSPstartpnt],trace[PSPstartpnt],'rs',ms=10)
-		axes.plot(tracetime[PSPendpnt],trace[PSPendpnt],'rd',ms=10)
-	        axes.plot(tracetime[plotstart:],trace[plotstart:],label=fileindex, color=((fileindex+11-goodtraces)*.1,0,(fileindex+11-goodtraces)*.1))
-            	axes.plot(tracetime[PP2startpnt],trace[PP2startpnt],'gd',ms=10)
+                axes.plot(tracetime[PSPstartpnt],trace[PSPstartpnt],'rs',ms=10)
+                axes.plot(tracetime[PSPendpnt],trace[PSPendpnt],'rd',ms=10)
+                axes.plot(tracetime[plotstart:],trace[plotstart:],label=fileindex, color=((fileindex+11-goodtraces)*.1,0,(fileindex+11-goodtraces)*.1))
+                axes.plot(tracetime[PP2startpnt],trace[PP2startpnt],'gd',ms=10)
         #if fileindex==2:
         #    break    
         #axes.plot(peaktime,peakvm[fileindex]-RMP[fileindex]+fileindex*0.001,'ko') #label the peak
@@ -315,7 +313,7 @@ datadict = dict(trace=tracedict, #datadict name wont be accessible outside this 
 #it includes all the experiment parameters (args)
 outfname=outputDir+args.experiment+"wcDATA"
 outpickle = outfname + '.pickle'
-with open(outpickle, 'w') as f:
+with open(outpickle, 'wb') as f:
 	pickle.dump(datadict, f) #saves the contents of datadict in file "f"... name datadict meaningless outside but contents (parameters, etc) retain meaning.
 
 #The next lines creates a text file for reading into igor (not needed to read into GrpAvg.py)
