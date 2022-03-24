4c075f29d87685bffd505a58745ca337f320960d
Avrama Blackwell <kblackw1@gmu.edu>
2022-03-24 13:59:18 -0400

    PopSpikeReExtractPeaks: use date/time stamp to determine units of parameters write file with expers with slope change, and expers with many extraction errors replace print with sys.exit if no files found

--- PopSpikeReExtractPeaks.py
+++ PopSpikeReExtractPeaks.py
@@ -3,21 +3,26 @@ import glob
 import pickle
 import sys
 import PopSpikeAnalClass as psc
+import os
+import time
+import datetime
 from argparse import Namespace
 import pop_spike_utilities as psu
 
 ms_per_sec=1000
+changedate=datetime.date(2021, 5, 12) #date file changed FVwidth and decay time units
 
 def find_files(subdir):
-	pattern = subdir+'202201*.pickle'
+	pattern = subdir+'20201224greendm105g1.pickle'
 	outfnames = sorted(glob.glob(pattern))
 	if not len(outfnames):
-		print('************ No files found *********')
+		sys.exit('************ No files found *********')
 	return outfnames
 	
 def analyze_files(fnames,new_outdir,do_exit):
 	#loop over all analyzed exepriments
 	slopechange=[]
+	expererrors=[]
 	for outfname in fnames:
 		with open(outfname, 'rb') as f:
 			#open pickle file
@@ -36,16 +41,31 @@ def analyze_files(fnames,new_outdir,do_exit):
 			theta=params.theta
 			commandline=[exper,sex,str(age),drug,str(theta),region]
 			params=psu.parse_args(commandline,do_exit,0)
+			print(params.datadir,glob.glob(params.datadir+params.exper+'*'),params.exper)
 			param_dict = vars(params)
 
 			#3. extract analysis parameters, store all needed parameters in params
+			#if (int(exper[0:8])<20210512) and (exper!='20160801green') and (exper[0:17]!='20160822orangedm1') and (exper!='20161121orange') and (exper!='20170119greendl5') and (exper!='20170120greendm5') and (exper!='20170324greendm105dmso') and (exper!='20170324greendmctrlnitr') and (exper!='20170324orangedmctrldmso') and (exper!='20170329greendm105nitr') and (exper!='20170329greendmctrldmso') and (exper!='20170329orangedm105dmso') and (exper!='20170405greendlctrlsch23390') and (exper!='20180315greendl105') and (exper!='20180612greendm5') and (exper!='20180916orangedm105') and (exper!='20181005orangedm5') and (exper!='20181117greendl5') and (exper!='20190104greendl5') and (exper!='20190110orangedm105') and (exper!='20190223greendm5') and (exper!='20190224orangedl105') and (exper!='20191211orangedm105mpp') and (exper!='20200113greendl5cycl') and (exper!='20200705orangedl5') and (exper[0:8]!='20200707') and (exper!='20200714greendm105') and (exper!='20201031orangedl5') and (exper!='20201220orangedmctrlg1') and (exper!='20201226orangedm105ppt'): #format of parameters changed on this date
+			if os.stat(outfname).st_mtime < time.mktime(changedate.timetuple()):
+				#if above fails, use os.path.basename(exper[0:8])
+				param_dict['decay']=datadict['anal_params']['artdecay']
+				param_dict['FVwidth']=datadict['anal_params']['FVwidth']
+				print('1st peak end fraction =',datadict['anal_params']['1st_peak_end'])
+			elif exper=='20180312orange1':
+				param_dict['decay']=datadict['anal_params']['artdecay']/ms_per_sec
+				param_dict['FVwidth']=datadict['anal_params']['FVwidth']/ms_per_sec
+				param_dict['first_peak_end_frac']=0.625
+			else:
+				param_dict['decay']=datadict['anal_params']['artdecay']*ms_per_sec
+				param_dict['FVwidth']=datadict['anal_params']['FVwidth']*ms_per_sec
+			if datadict['anal_params']['1st_peak_end']<0.5: #this number is arbitrary. maybe use 0.625?
+				param_dict['first_peak_end_frac']=0.625
+			else:
+				param_dict['first_peak_end_frac']=datadict['anal_params']['1st_peak_end']
 			param_dict['base_min']=datadict['anal_params']['baseline_min']
 			param_dict['noisethres']=datadict['anal_params']['noisethresh']
-			param_dict['decay']=datadict['anal_params']['artdecay']*ms_per_sec
-			param_dict['FVwidth']=datadict['anal_params']['FVwidth']*ms_per_sec
 			param_dict['artthresh']=datadict['anal_params']['artifactthresh']
 			param_dict['art_win']=datadict['anal_params']['artifact_wind']
-			param_dict['first_peak_end_frac']=datadict['anal_params']['1st_peak_end']
 			param_dict['induction_gap_time']=datadict['anal_params']['induction_gap']
 			param_dict['baselinepoints']=datadict['anal_params']['baselinepts']
 
@@ -60,11 +80,14 @@ def analyze_files(fnames,new_outdir,do_exit):
 			
 			pop_spike.find_induction_gap()
 			pop_spike.findPopSpike()
+			if len(pop_spike.problemtraces['tracenum'])>5:
+				param_dict['error_number']=len(pop_spike.problemtraces['tracenum'])
+				expererrors.append(param_dict)
 			pop_spike.normalizeAmp()
 			pop_spike.summaryMeasure()
 			print('PREVIOUS SLOPE for', params.exper,'=',old_slope,'+/-',old_slope_std)
 			pop_spike.showSummaryPlot(plot=True)
-			if np.abs((old_slope-pop_spike.Bopt)/old_slope)>0.1: #percent change from old slope
+			if (np.abs(old_slope)>0.01 and np.abs(pop_spike.Bopt)<0.01) or (np.abs(old_slope)<0.01 and np.abs(pop_spike.Bopt)>0.01): #percent change from old slope
 				param_dict['old slope']=old_slope
 				param_dict['new slope']=round(pop_spike.Bopt,6)
 				slopechange.append(param_dict)
@@ -73,7 +96,17 @@ def analyze_files(fnames,new_outdir,do_exit):
 			pop_spike.saveData()  #save data in new directory called, e.g. "fixed baseline"
 			#create additional file that lists experiments in which original baseline was good, and now is not
 			# or original baseline was bad, and now is good
-	return slopechange
+	return expererrors,slopechange
+
+def writefile(variables,datatoprint,filename):
+	header='   '.join(variables)
+	f=open(filename,'w')
+	f.write(header+'\n')
+	for exper in datatoprint:
+		newline='   '.join([str(exper[k]) for k in variables])
+		f.write(newline+'\n')
+	f.close()
+	return
 
 ARGS='C:\\Users\\vlewitus\\Documents\\Python_Scripts\\Pickle\\ C:\\Users\\vlewitus\\Documents\\Python_Scripts\\Pickle_new_baseline\\'			
 if __name__=='__main__':
@@ -87,17 +120,16 @@ if __name__=='__main__':
 pickledir=commandline[0]
 new_outdir=commandline[1]
 pickle_files=find_files(pickledir)
-slopechange=analyze_files(pickle_files,new_outdir,do_exit)
+expererrors,slopechange=analyze_files(pickle_files,new_outdir,do_exit)
+
+######### Write text file of experiments that have significant slope changes ####
 if len(slopechange):
 	print(slopechange)
-	######### Write text file of experiments that have significant slope changes ####
 	variables=['exper','old slope','new slope','sex','age','drug','theta','region','decay','artthresh','FVwidth','first_peak_end_frac','base_min','noisethres']
-	header='   '.join(variables)
-	f=open('slopechange.txt','w')
-	f.write(header+'\n')
-	for exper in slopechange:
-		newline='   '.join([str(exper[k]) for k in variables])
-		f.write(newline+'\n')
-	f.close()
+	writefile(variables,slopechange,'slopechange.txt')
 else:
 	print('NO MAJOR SLOPE CHANGE')
+######### Write text file of experiments that have many errors ####
+if len(expererrors):
+	variables=['exper','error_number','sex','age','drug','theta','region','decay','artthresh','FVwidth','first_peak_end_frac','base_min','noisethres']
+	writefile(variables,expererrors,'expererrors.txt')
